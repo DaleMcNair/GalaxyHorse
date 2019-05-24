46843f2cf7e9013c3e565594956d3e1a85de712a
Unknown <dockit@gmail.com>
2019-05-24 14:55:43 +1000

    React helmet usage, fix sass bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from "nes-react";
 import * as React from "react";
+import Helmet from "react-helmet";
 import "../layouts/index.scss";
 
 // Please note that you can use https://github.com/dotansimha/graphql-code-generator
@@ -21,6 +22,17 @@ export default class extends React.Component<IndexPageProps, {}> {
   public render() {
     return (
       <Container>
+        <Helmet
+          title="Galaxy Horse"
+          meta={[
+            {
+              name: "description",
+              content:
+                "Galaxy Horse. Games; galactically obtuse creations which are of equine origin since 2017."
+            },
+            { name: "keywords", content: "Video Games, Galaxy Horse, " }
+          ]}
+        />
         <div id="stars" />
         <div id="stars2" />
         <div id="stars3" />
